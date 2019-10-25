6a5662cfe36ac4c6356d63002ffd5b04f7055ae2
sfhuictk <lvlei1919@sina.com>
2019-10-25 23:46:32 +0800

    home

--- src/pages/list/basic-list/basic/service.ts
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request';
-
-export async function queryBasicProfile() {
-  return request('/api/profile/basic');
-}
-
-export async function queryDispatch(id: number = 1) {
-  return request('/server/api/dispatch/' + id);
-}
